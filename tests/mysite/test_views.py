0a95c8159191135e6c862c86e2a3b1b70141a77d
Vishal <gargv@sheridancollege.ca>
2022-10-02 15:59:57 -0400

    Commit

--- tests/mysite/test_views.py
+++ tests/mysite/test_views.py
@@ -1,3 +1,8 @@
+# test_views.py
+
 def test_index_ok(client):
-    response = client.get('/banana')  # Doesn't exist
+    # Make a GET request to / and store the response object
+    # using the Django test client.
+    response = client.get('/')
+    # Assert that the status_code is 200 (OK)
     assert response.status_code == 200
